3b9b7d92e61f388ae4caf232529742f50eea90da
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-27 20:42:30 +0700

    feat: add public pages

--- src/_public/PublicLayout.tsx
+++ src/_public/PublicLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-export default function publicLayout() {
+export default function PublicLayout() {
   return (
     <>
       <Outlet />
--- src/_public/pages/LoginPage.tsx
+++ src/_public/pages/LoginPage.tsx
@@ -1,7 +1,3 @@
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,44 +6,48 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useLoginMutation } from "@/action/loginMutation";
+import { useLoginMutation } from "@/action/useLogin";
+import { useUserContext } from "@/context/useUserContext";
+import { loginSchema } from "@/constant";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { AxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6, {
-    message: "Password must be at least 6 characters",
-  }),
-});
-
 export default function LoginPage() {
   const { mutateAsync: login, isPending } = useLoginMutation();
   const navigate = useNavigate();
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { onAuthSuccess } = useUserContext();
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  async function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
     try {
       const res = await login(values);
-      console.log(res);
+
+      localStorage.setItem("token", res.data.token);
+      onAuthSuccess({ payload: res.data.payload });
       navigate("/dashboard");
     } catch (error) {
       const err = error as AxiosError<{ message: string }>;
-      toast.error(err.response?.data.message);
+      toast.error(err.response?.data.message || err.message);
     }
   }
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col max-w-md w-full rounded-lg shadow-lg">
+    <div className="flex justify-center items-center h-screen bg-red-500">
+      <div className="flex flex-col max-w-md w-full rounded-lg shadow-lg bg-white">
         <div className="p-2 text-center font-bold text-2xl border-b rounded-t-lg ">
           LOGIN
         </div>
@@ -65,7 +65,6 @@ export default function LoginPage() {
                   <FormControl>
                     <Input placeholder="email" type="text" {...field} />
                   </FormControl>
-
                   <FormMessage />
                 </FormItem>
               )}
@@ -79,14 +78,13 @@ export default function LoginPage() {
                   <FormControl>
                     <Input placeholder="password" type="password" {...field} />
                   </FormControl>
-
                   <FormMessage />
                 </FormItem>
               )}
             />
             <Button
               type="submit"
-              className="w-full bg-black hover:opacity-75 text-white"
+              className="w-full bg-yellow-300 hover:opacity-75 "
               disabled={isPending}
             >
               Submit
