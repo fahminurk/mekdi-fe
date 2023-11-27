3655372061072766c4c221d0bad4918ce28d65b6
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-27 20:43:43 +0700

    refactor: update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import "./globals.css";
 import { Routes, Route } from "react-router-dom";
 import PublicLayout from "./_public/PublicLayout";
-import { LoginPage } from "./_public/pages";
-import "./globals.css";
+import { HomePage, LoginPage } from "./_public/pages";
 import { Toaster } from "sonner";
+import PrivateLayout from "./_private/PrivateLayout";
+import { DashboardPage, ProductPage, UserPage } from "./_private/pages";
 function App() {
   return (
     <main>
@@ -10,10 +12,17 @@ function App() {
         {/* public Routes */}
         <Route element={<PublicLayout />}>
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
         </Route>
 
         {/* private Routes */}
+        <Route element={<PrivateLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="/user" element={<UserPage />} />
+        </Route>
       </Routes>
+
       <Toaster />
     </main>
   );
