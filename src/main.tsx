d570ad49935bcc2170ebf163d60881ce3dd8ccd4
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-27 20:45:21 +0700

    refactor: update main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query.ts";
+import { AuthProvider } from "./provider/AuthProvider.tsx";
+import ModalProvider from "./provider/ModalProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <ModalProvider />
+        <App />
+      </QueryClientProvider>
+    </AuthProvider>
   </BrowserRouter>
 );
